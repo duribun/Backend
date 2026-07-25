390ed8849f3c127deff4a227bf14719139ddf550
송성호 <thdtjdgh1679@naver.com>
2026-07-25 20:56:42 +0900

    feat(global): 전역 예외 처리 및 커스텀 예외 추가
    - GlobalExceptionHandler, ErrorResponse 추가
    - InvalidSocialTokenException, InvalidRefreshTokenException, UnsupportedProviderException 추가

---
+++ src/main/java/duribun/be/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package duribun.be.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidSocialTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidSocialToken(InvalidSocialTokenException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRefreshToken(InvalidRefreshTokenException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(UnsupportedProviderException.class)
+    public ResponseEntity<ErrorResponse> handleUnsupportedProvider(UnsupportedProviderException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .orElse("잘못된 요청입니다");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(message));
+    }
+}
---
+++ src/main/java/duribun/be/global/exception/UnsupportedProviderException.java
@@ -0,0 +1,8 @@
+package duribun.be.global.exception;
+
+public class UnsupportedProviderException extends RuntimeException {
+
+    public UnsupportedProviderException(String message) {
+        super(message);
+    }
+}
