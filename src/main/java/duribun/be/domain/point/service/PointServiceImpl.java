9db3fb184994ec76ec2127221574e949f89b0f19
송성호 <thdtjdgh1679@naver.com>
2026-09-05 23:44:03 +0900

    feat(point): 마스코트/칭호 지급 리스너를 전용 클래스로 분리하고 BADGE_ACQUIRED로 확정

--- src/main/java/duribun/be/domain/point/service/PointServiceImpl.java
+++ src/main/java/duribun/be/domain/point/service/PointServiceImpl.java
@@ -1,14 +1,11 @@
 package duribun.be.domain.point.service;
 
-import duribun.be.domain.badge.event.BadgeAcquiredEvent;
-import duribun.be.domain.mascot.event.MascotAcquiredEvent;
 import duribun.be.domain.point.dto.PointHistoryResponse;
 import duribun.be.domain.point.entity.PointAccount;
 import duribun.be.domain.point.entity.PointHistory;
 import duribun.be.domain.point.repository.PointAccountRepository;
 import duribun.be.domain.point.repository.PointHistoryRepository;
 import duribun.be.global.exception.InsufficientPointException;
-import org.springframework.context.event.EventListener;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,9 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class PointServiceImpl implements PointService {
 
-    private static final int MASCOT_ACQUIRED_REWARD = 20;
-    private static final int BADGE_ACQUIRED_REWARD = 50;
-
     private final PointAccountRepository pointAccountRepository;
     private final PointHistoryRepository pointHistoryRepository;
 
@@ -29,16 +23,6 @@ public class PointServiceImpl implements PointService {
                              PointHistoryRepository pointHistoryRepository) {
         this.pointAccountRepository = pointAccountRepository;
         this.pointHistoryRepository = pointHistoryRepository;
-    }
-
-    @EventListener
-    public void handleMascotAcquired(MascotAcquiredEvent event) {
-        earn(event.userId(), MASCOT_ACQUIRED_REWARD, PointReason.MASCOT_COLLECT);
-    }
-
-    @EventListener
-    public void handleBadgeAcquired(BadgeAcquiredEvent event) {
-        earn(event.userId(), BADGE_ACQUIRED_REWARD, PointReason.BADGE_COLLECT);
     }
 
     @Override
