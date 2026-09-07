7029f818f1e26bce94e93e10ee3fb4ff024e9821
sungho1949 <thdtjdgh1679@naver.com>
2026-09-07 14:09:07 +0000

    refactor(user): 프로필/닉네임 API를 setting에서 user 도메인으로 이관
    - UserProfileController, NicknameCheckResponse/ProfileUpdateRequest/ProfileUpdateResponse를
      domain/setting -> domain/user로 이동
    - SettingService에서 프로필/닉네임 위임 메서드 제거 (컨트롤러가 UserService를 직접 호출)
    - API 라우트/요청/응답 스펙 변경 없음

--- src/main/java/duribun/be/domain/setting/service/SettingService.java
+++ src/main/java/duribun/be/domain/setting/service/SettingService.java
@@ -1,13 +1,10 @@
 package duribun.be.domain.setting.service;
 
 import duribun.be.domain.auth.service.AuthService;
-import duribun.be.domain.setting.dto.ProfileUpdateRequest;
-import duribun.be.domain.setting.dto.ProfileUpdateResponse;
 import duribun.be.domain.setting.dto.UserSettingResponse;
 import duribun.be.domain.setting.dto.WithdrawResponse;
 import duribun.be.domain.setting.entity.UserSetting;
 import duribun.be.domain.setting.repository.UserSettingRepository;
-import duribun.be.domain.user.entity.User;
 import duribun.be.domain.user.entity.UserStatus;
 import duribun.be.domain.user.service.UserService;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -44,15 +41,6 @@ public class SettingService {
         userService.withdraw(userId);
         authService.revokeAllTokens(userId);
         return WithdrawResponse.from(UserStatus.WITHDRAWN);
-    }
-
-    public boolean checkNicknameAvailability(Long userId, String nickname) {
-        return userService.isNicknameAvailable(userId, nickname);
-    }
-
-    public ProfileUpdateResponse updateProfile(Long userId, ProfileUpdateRequest request) {
-        User user = userService.updateProfile(userId, request.nickname(), request.birthDate(), request.gender());
-        return ProfileUpdateResponse.from(user);
     }
 
     private UserSetting findOrCreateSetting(Long userId) {
